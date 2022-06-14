507ca0bf758aa40dc03cc3b4b22014ea35f85153
Zsolt Kiss <56066989+Zsolti0819@users.noreply.github.com>
2022-06-14 13:51:00 +0200

    Presentational and Container Components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -2,42 +2,28 @@ import React, {Component} from "react";
 import {Card, CardBody, CardImg, CardText, CardTitle} from "reactstrap";
 
 class DishDetail extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            selectedDish: null
-        }
-        console.log('DishDetail Component constructor is invoked');
-    };
 
     renderDish(dish) {
-        if (dish != null) {
-            return (
-                <Card>
-                    <CardImg width="100%" src={dish.image} alt={dish.name} />
-                    <CardBody>
-                        <CardTitle>{dish.name}</CardTitle>
-                        <CardText>{dish.description}</CardText>
-                    </CardBody>
-                </Card>
-            )
-        }
-        else {
-            return (
-                <div></div>
-            );
-        }
+        return (
+            <Card>
+                <CardImg width="100%" src={dish.image} alt={dish.name} />
+                <CardBody>
+                    <CardTitle>{dish.name}</CardTitle>
+                    <CardText>{dish.description}</CardText>
+                </CardBody>
+            </Card>
+        )
+
     }
 
     renderComments(dish) {
         return dish.comments.map((cm) => {
             return (
-                <div key = {cm.id}>
+                <div>
                     <CardText>{cm.comment}</CardText>
                     <CardText>-- {cm.author}, {new Intl.DateTimeFormat("en-US", {
                         year: "numeric",
-                        month: "long",
+                        month: "short",
                         day: "2-digit",
                     }).format(new Date(cm.date))}</CardText>
                 </div>
@@ -46,22 +32,26 @@ class DishDetail extends Component {
     }
 
     render() {
-        const selected = this.props.selectedDish;
 
-        return (
-            <div className="container">
-                <div className="row">
-                    <div key = {selected.id} className="col-12 col-md-5 m-1">
-                        {this.renderDish(selected)}
-                    </div>
-                    <div className="col-12 col-md-5 m-1">
-                        <h4><b>Comments</b></h4>
-                        {this.renderComments(selected)}
+        if (this.props.dish != null) {
+            const selected = this.props.dish;
+            return (
+                <div className="container">
+                    <div className="row">
+                        <div className="col-12 col-md-5 m-1">
+                            {this.renderDish(selected)}
+                        </div>
+                        <div className="col-12 col-md-5 m-1">
+                            <h4><b>Comments</b></h4>
+                            {this.renderComments(selected)}
+                        </div>
                     </div>
                 </div>
-            </div>
-
-        )
+            )
+        }
+        else {
+            return (<div></div>);
+        }
     }
 }
 
