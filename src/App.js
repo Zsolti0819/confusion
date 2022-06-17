152f4e9e5ecff507314f3fdb2f23128e96a4763b
Zsolt Kiss <56066989+Zsolti0819@users.noreply.github.com>
2022-06-17 23:00:00 +0200

    React Router

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import './App.css';
-import './components/MainComponent'
-import {Component} from "react";
+import React, { Component } from "react";
 import Main from "./components/MainComponent";
+import './App.css';
+import { BrowserRouter} from "react-router-dom";
 
 class App extends Component {
 
     render() {
         return (
-            <div>
-                <Main />
-            </div>
+            <BrowserRouter>
+                <div>
+                    <Main />
+                </div>
+            </BrowserRouter>
         );
     }
-
 }
 
 export default App;
