47b1310b41be2e81c369273c70564b05cdcb93b8
Zsolt Kiss <56066989+Zsolti0819@users.noreply.github.com>
2022-06-21 11:07:52 +0200

    Controlled Forms

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Breadcrumb, BreadcrumbItem, Card, CardBody, CardImg, CardText, CardTitle} from "reactstrap";
+import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardImg, CardText, CardTitle } from "reactstrap";
 import { Link } from 'react-router-dom';
 
     function RenderDish({dish}) {
